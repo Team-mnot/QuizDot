c05e25404fbcf6d7db20b39fd558cdaeddd3b4a0
37jsbean <37jsbean@naver.com>
2024-05-04 06:38:28 +0900

    [FE] feat: 로비 websocket 기초 연결

--- quizdot-client/src/pages/channel/components/Page.tsx
+++ quizdot-client/src/pages/channel/components/Page.tsx
@@ -1,16 +1,27 @@
 import { useEffect } from 'react';
 import { Button } from '@/shared/ui';
+import { useNavigate } from 'react-router-dom';
 
 export function ChannelPage() {
+  const navi = useNavigate();
+
   useEffect(() => {
     document.body.style.backgroundImage = 'url(/images/main_bg.png)';
     document.body.style.backgroundSize = 'cover';
   }, []);
 
+  const handleClickChannel = (channel: number) => {
+    navi(`/${channel}/lobby`, { replace: true });
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen text-center">
+    <div
+      className={
+        'flex h-screen w-screen flex-col items-center justify-center text-center'
+      }
+    >
       <h1>채널</h1>
-      <Button value={'1채널'} />
+      <Button value={'1채널'} onClick={() => handleClickChannel(1)} />
       <Button value={'2채널'} />
       <Button value={'3채널'} />
       <Button value={'4채널'} />
--- quizdot-client/src/pages/lobby/components/Page.tsx
+++ quizdot-client/src/pages/lobby/components/Page.tsx
@@ -1,17 +1,105 @@
-import { ChattingBox } from '@/shared/ui/ChattingBox';
 import { RoomListComponent } from './RoomListComponent';
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { OnlineUserListComponent } from './OnlineUserListComponent';
 import { MyProfileComponent } from './MyProfileComponent';
 
+import SockJS from 'sockjs-client/dist/sockjs';
+import { Stomp, CompatClient } from '@stomp/stompjs';
+
+import { useParams } from 'react-router-dom';
+import { Button, Input } from '@/shared/ui';
+
+interface MessageDto {
+  sender: string;
+  text: string;
+  type: string;
+  data: unknown;
+}
+
 export function LobbyPage() {
+  const { channel } = useParams() as { channel: string };
+
+  const client = useRef<CompatClient | null>(null);
+
+  // 첫 접속 시 Socket 생성해 연결
+  function onConnect(): void {
+    const socket = new SockJS(`https://k10d102.p.ssafy.io/api/ws/chat`);
+
+    client.current = Stomp.over(socket);
+    client.current.connect({}, onSubscribe);
+  }
+
+  // Socket 연결 시 실행
+  function onSubscribe(): void {
+    try {
+      // Subscribe 함수 (CallBack 함수를 포함)
+      client.current?.subscribe(
+        `/sub/chat/lobby/${channel}`,
+
+        (message) => {
+          const msg: MessageDto = JSON.parse(message.body) as MessageDto;
+          setMessages([...messages, msg.text]);
+          console.error('[콜백 성공] server -> client');
+        },
+      );
+    } catch (error: unknown) {
+      onError(error);
+
+      // 연결에 실패하면 이전 채널로 돌아갈 것인지 팝업창으로 묻고, OK 하면 돌아감
+      // navi(`/channel`, { replace: true });
+    }
+  }
+
+  // Socket 연결 도중 Error 발생 시 실행될 함수
+  function onError(error: unknown): void {
+    console.error('[연결 실패] WebSocket Connection Error', error);
+  }
+
+  // Send 함수
+  function onSend(): void {
+    if (message.trim() != '' && client) {
+      const chatMessage = {
+        sender: '익',
+        text: message,
+        type: null,
+        data: null,
+      };
+
+      client.current?.send(
+        `/pub/chat/lobby/${channel}`,
+        {},
+        JSON.stringify(chatMessage),
+      );
+
+      setMessage('');
+      console.log('[연결 성공] Client -> Server');
+    }
+  }
+
+  // function onUnsubscribe(): void {
+  //   client.current?.unsubscribe;
+  // }
+
+  // function onDisconnect(): void {
+  //   client.current?.onDisconnect;
+  // }
+
+  const [message, setMessage] = useState<string>(''); // 사용자 입력을 상태로 관리
+  const [messages, setMessages] = useState<string[]>([]);
+
   useEffect(() => {
-    document.body.style.backgroundImage = 'url(/images/main_bg.png)';
-    document.body.style.backgroundSize = 'cover';
+    onConnect();
   }, []);
 
   return (
-    <div>
+    <div
+      style={{
+        backgroundImage: 'url(/images/main_bg.png)',
+        backgroundSize: 'cover',
+        width: '100%',
+        height: '100vh', // 혹은 다른 높이 값
+      }}
+    >
       <div>
         <h1>로비</h1>
         <div className={'flex'}>
@@ -20,7 +108,26 @@ export function LobbyPage() {
         </div>
         <div className={'flex'}>
           <MyProfileComponent />
-          <ChattingBox />
+          <div>
+            {messages.map((item) => (
+              <div key={item}>{item}</div>
+            ))}
+          </div>
+          <Input
+            value={message}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setMessage(e.currentTarget.value)
+            }
+          />
+          <Button
+            value="전송"
+            onClick={() => {
+              onSend();
+              alert('전송완료');
+            }}
+          />
+
+          {/* <ChattingBox /> */}
         </div>
       </div>
     </div>
