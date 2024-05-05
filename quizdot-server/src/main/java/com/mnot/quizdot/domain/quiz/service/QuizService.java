80708d3f09dd3dbbb9fad396257152fd63169413
최부광 <cbgbg0727@knu.ac.kr>
2024-05-05 21:36:57 +0900

    [BE] feat: 게임 시작 API 구현

--- quizdot-server/src/main/java/com/mnot/quizdot/domain/quiz/service/QuizService.java
+++ quizdot-server/src/main/java/com/mnot/quizdot/domain/quiz/service/QuizService.java
@@ -1,6 +1,7 @@
 package com.mnot.quizdot.domain.quiz.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.mnot.quizdot.domain.member.entity.ModeType;
 import com.mnot.quizdot.domain.quiz.dto.QuizListRes;
 import com.mnot.quizdot.domain.quiz.dto.QuizParam;
 
@@ -10,5 +11,9 @@ public interface QuizService {
 
     void passQuestion(int roomId, int questionId, String memberId, String nickname)
         throws JsonProcessingException;
+
+    void startGame(int roomId, int memberId, ModeType mode);
+
+    void deleteGame(int roomId);
 
 }
