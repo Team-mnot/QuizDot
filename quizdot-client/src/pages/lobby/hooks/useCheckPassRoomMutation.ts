538e45e83ac1a8dc3fa0150bad8e816d3764bb6f
37jsbean <37jsbean@naver.com>
2024-05-08 13:18:19 +0900

    [FE] fix: 로비 useMutation 빈 파일 삭제

--- quizdot-client/src/pages/lobby/hooks/useCheckPassRoomMutation.ts
+++
@@ -1,9 +0,0 @@
-import { useQueryClient } from "@tanstack/react-query";
-
-export function useEnterRoomMutation() {
-    const client = useQueryClient();
-
-   return useMutation{(
-        mutationFn: (postId) => Enter(postId),
-    )};
-};
